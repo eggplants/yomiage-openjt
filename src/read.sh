810ac0998ff3c8195e731e9baf57b4668b862e8d
eggplants <w10776e8w@yahoo.co.jp>
2021-10-02 08:41:30 +0900

    fix

--- src/read.sh
+++ src/read.sh
@@ -18,55 +18,101 @@ chkcmd() {
 }
 
 yomi() {
-  cat - |
-    open_jtalk \
-      -x /var/lib/mecab/dic/open-jtalk/naist-jdic \
-      -m "$1" \
-      -ow /dev/stdout -r 0.8 |
-    aplay --quiet
+    cat - |
+      open_jtalk \
+        -x /var/lib/mecab/dic/open-jtalk/naist-jdic \
+        -m "$1" \
+        -s 48000 \
+        -ow "$TMP/$(uuidgen).wav" -r 0.8
+}
+
+usage() {
+  echo "Usage: $0 [-s] [-h] [<file>, def: /dev/stdin] [<actor>, def: 白狐舞]"
+  exit "${1-0}"
 }
 
 main() {
-  chkcmd open_jtalk aplay
+  chkcmd open_jtalk aplay uuidgen sox
   trap 'kill $$' 1 2 3 15
-  local f a hts word_count
+  local SAVE
+  while getopts "sh" OPT
+  do
+    case "$OPT" in
+      s) SAVE="true" ;;
+      h) usage ;;
+      *) echo "option '${OPT}' is invalid." >&2 && usage 1
+         ;;
+    esac
+  done
+  shift "$((OPTIND - 1))"
+  local f a
   f="${1-/dev/stdin}"
   a="${2-白狐舞}"
-
+  echo "[input: $f, actor: $a]"
   # check given input file
-  if [ "$f" = '-' ]; then
+  if [ "$f" = '-' ] || [ "$f" = '/dev/stdin' ]; then
+    if ! [ -f "/dev/stdin" ]; then
+      echo "stdin is empty">&2
+      exit 1
+    fi
     f="/dev/stdin"
   fi
   if ! [ -f "$f" ]; then
     echo "file '$f' is not file"
-    exit 1
+    usage 1
   fi
+  local word_count
   word_count="$(wc -c < "$f")"
   if [ "$word_count" -eq 0 ]; then
     echo "file '$f' is empty" >&2
-    exit 1
+    usage 1
   fi
 
   # check given actor htsvoice
+  local hts
   hts="$(
     find /usr/share/hts-voice/ -name "${a}.htsvoice" -type f |
       head -1
   )"
   if [ -z "$hts" ]; then
-    echo "actor '$f' is not found" >&2
-    exit 1
+    echo "actor '$a' is not found" >&2
+    usage 1
   fi
+
+  readonly OUT="audio_${f//\//-}_$(date +%s).wav"
+  readonly TMP="$(mktemp -d)"
+  echo "[working in: $TMP]"
+  local line_count idx
+  line_count="$(< "$f" wc -l)"
+  idx=0
   grep -vE "^#" "$f" |
     while read -r line; do
+      ((idx++))
+      echo -ne "\r[${idx}/${line_count}]"
       if [[ "$line" =~ *min$ ]]; then
-        sleep "${line//min/}m"
-      elif [[ "$line" = "" ]]; then
-        sleep 0.3
+        local m
+        m="${line//min/}"
+        sox -n -r 48000 -c 1 "$TMP/$(uuidgen).wav" trim 0 "$((m*60))"
+      elif [[ -z "$line" ]]; then
+        sox -n -r 48000 -c 1 "$TMP/$(uuidgen).wav" trim 0 0.3
       else
         echo "$line" | yomi "$hts" 2> /dev/null
       fi
       sleep 0.01
     done
+
+  echo
+  ls "$TMP"/*.wav -tr > "$TMP/_list"
+  readarray -t arr < "$TMP/_list"
+  if sox "${arr[@]}" "$TMP/$OUT"; then
+    if [[ "$SAVE" = "true" ]]; then
+      mv "$TMP/$OUT" .
+      echo "[created: $OUT]"
+      aplay "$OUT"
+    else
+      aplay "$TMP/$OUT"
+    fi
+  fi
 }
 
 main "$@"
